460fe6f9a033e21eac3760885e7330fe6a58fa03
Kaique Beraguas <kaiqueberaguas@gmail.com>
2019-09-14 02:04:18 -0300

    Commit inicial

---
+++ ConsoleEntrada/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleEntrada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
