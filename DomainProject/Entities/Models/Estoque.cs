4c5946c2855aabf75ac01ed115f30a1370d2e678
Kaique Beraguas <kaique.beraguas@gmail.com>
2019-10-07 01:00:31 -0300

    Migração do projeto para dotnet core 3.0

--- DomainProject/Entities/Models/Estoque.cs
+++ DomainProject/Entities/Models/Estoque.cs
@@ -7,9 +7,12 @@ namespace DomainProject.Entities.Models
     class Estoque
     {
         public int Id { get; set; }
-        public Produto Prduto { get; set; }
+
+        public int ProductId { get; set; }
+        public  virtual Produto Prduto { get; set; }
         public int Quantidade { get; set; }
-        public Local Local { get; set; }
+        public virtual Local Local { get; set; }
+        public int LocalId { get; set; }
         public DateTime Chegada { get; set; }
         public DateTime Validade { get; set; }
     }
---
+++ DotnetSotre.Infra.Data/Context/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetSotre.Infra.Data.Context
+{
+    public class StoreContext : DbContext
+    {
+
+    }
+}
